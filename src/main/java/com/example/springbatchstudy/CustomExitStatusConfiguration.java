04f2ad7a960d9a3c64a49058139edbc31320c5e7
moon-mac <bbangssi91@gmail.com>
2022-08-05 13:58:08 +0900

    1.4-custom exit status

--- src/main/java/com/example/springbatchstudy/CustomExitStatusConfiguration.java
+++ src/main/java/com/example/springbatchstudy/CustomExitStatusConfiguration.java
@@ -6,36 +6,26 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.builder.FlowBuilder;
-import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
 @Configuration
-public class TransitionConfiguration {
+public class CustomExitStatusConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
     public Job batchJob(){
-        // 총 3개의 Flow
         return jobBuilderFactory.get("batchJob")
                 .start(step1())
                     .on("FAILED")
                     .to(step2())
-                    .on("FAILED")
+                    .on("PASS")
                     .stop()
-                .from(step1())
-                    .on("*")
-                    .to(step3())
-                    .next(step4())
-                .from(step2())
-                    .on("*")
-                    .to(step5())
-                    .end()
+                .end()
                 .build();
     }
 
@@ -54,27 +44,9 @@ public class TransitionConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
+                .listener(new PassCheckingListener())
                 .build();
     }
 
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
-                .build();
-    }
 
-    @Bean
-    public Step step4() {
-        return stepBuilderFactory.get("step4")
-                .tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
-                .build();
-    }
-
-    @Bean
-    public Step step5() {
-        return stepBuilderFactory.get("step5")
-                .tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
-                .build();
-    }
 }
