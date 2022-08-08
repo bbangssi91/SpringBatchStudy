e643128a9b4befba339661e9749df699e6d32592
moon-mac <bbangssi91@gmail.com>
2022-08-08 18:24:28 +0900

    3.1-JDBC Cursor Item Reader

---
+++ src/main/java/com/example/springbatchstudy/JdbcCursorConfiguration.java
@@ -0,0 +1,69 @@
+package com.example.springbatchstudy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@RequiredArgsConstructor
+@Configuration
+public class JdbcCursorConfiguration {
+
+    private final int chunkSize = 10;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
+
+    @Bean
+    public Job batchJob(){
+        return jobBuilderFactory.get("batchJob")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Customer, Customer>chunk(chunkSize)
+                .reader(customItemReader())
+                .writer(customItemWriter())
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((stepContribution, chunkContext) -> RepeatStatus.FINISHED)
+                .build();
+    }
+
+    @Bean
+    public ItemWriter<? super Customer> customItemWriter() {
+        System.out.println("==================");
+        return items -> items.forEach(System.out::println);
+    }
+
+    @Bean
+    public ItemReader<? extends Customer> customItemReader() {
+        return new JdbcCursorItemReaderBuilder<Customer>()
+                .name("jdbcCursorItemReader")
+                .fetchSize(chunkSize)
+                .sql("select id, firstName, lastName, birthdate from customer where firstName like ? order by lastName, firstName")
+                .beanRowMapper(Customer.class)
+                .queryArguments("A%")
+                .dataSource(dataSource)
+                .build();
+    }
+
+
+}
