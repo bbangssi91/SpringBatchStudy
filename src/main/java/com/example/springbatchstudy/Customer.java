75003cb370769f98cb546de575d52952af77bc06
moon-mac <bbangssi91@gmail.com>
2022-08-09 13:45:30 +0900

    3.2-JDBC Paging Item Reader

---
+++ src/main/java/com/example/springbatchstudy/Customer.java
@@ -0,0 +1,12 @@
+package com.example.springbatchstudy;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String birthdate;
+}
